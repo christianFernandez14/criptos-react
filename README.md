bb126e6df6ff1926e90af76a28983383fdd3a2e4
Christian Fernandez <christian14@gmail.com>
2023-11-28 15:45:30 -0300

    Creando proyecto y realizando algunos ajuste basicos

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 1.- Vamos a crear nuestro propio Hooks
 2.- Como focus Css: Styled Components
 3.- Consumir info desde un Api
+
+Inciando el Proyecto:
+
+1.- Ubicas el lugar donde arrancara tu proyecto
+2.- Desde tu bash, powershel, cmd o terminal de VisualScode: 
+    => npm init vite@latest (tampien puedes ejecutar nom create vite@latest)
+    => Nombre del proyecto
+    => le das la opcion de React (ya que hay otroas librerias o frameworks con que trabaja Vite tambien)
+    => Luego a JavaScrip
+    => Luego le das cd nombre_del_proyecto
+    => npm i (para que instale las dependecias -node_modules)
+    => npm run dev (para levantar en proyecto en el puerto que te indico)
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
+    /* 2 */
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Desde App</h1>
     </>
   )
 }
 
 export default App
+
+/* 
+CONFIGURACIONES BASICAS - INICIO DE TU ENTORNO DE TRABAJO
+
+1.- Eliminamos todos los logos que existian en el proyecto
+2.- Eliminamos el App.css y todo el contenido que existian dentro de este componente
+3.- Eliminamos todo el contenido que habia dentro del index.css y le dimos una sola propiedad (back ground, para el
+    uso exclusivo de este proyecto)
+4.- Incorporamos una carpeta dentro del /SRC, "img"; que solo lleva una imagen, es para uso del proyecto,
+*/
