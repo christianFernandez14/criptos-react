f29177d4aa3db9a2b4be66e52e3439bb19663984
Christian Fernandez <christian14@gmail.com>
2023-11-29 10:55:06 -0300

    Incorporando un segundo Select con el hook useSelectMonedas()

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,3 @@
-/* 6 */
 import { useState, useEffect } from "react";
 
 import useSelectMonedas from "../hooks/useSelectMonedas"
@@ -24,38 +23,29 @@ const InputSubmit = styled.input`
   }
 `
 const Formulario = () => {
-  
-  /* 7 */
+
   const [criptos, setCriptos] = useState([])
 
   const [moneda, SelectMonedas] = useSelectMonedas('Elige tu Moneda', monedas);
+  /* 1 */
+  const [criptomonedas, SelectCriptomonedas] = useSelectMonedas('Elige tu Criptomoneda', criptos);
 
   useEffect(() => {
 
     const consultarAPI = async () => {
-      const URL = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
+      const URL = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD'
       const respuesta = await fetch(URL)
       const resultado = await respuesta.json()
 
-      /* 1 */
       const arrayCripto = resultado.Data.map(cripto => {
 
-        /* 3 */
         const objetoCripto = {
           id: cripto.CoinInfo.Name,
           nombre: cripto.CoinInfo.FullName
         }
-
-        /* 2 */
-        // console.log(cripto.CoinInfo.Name, cripto.CoinInfo.FullName)
-
-        /* 4 */
         return objetoCripto
       })
-      /* 5 */
-      console.log(arrayCripto)
 
-      /* 8 */
       setCriptos(arrayCripto)
     }
 
@@ -66,6 +56,9 @@ const Formulario = () => {
   return (
     <form>
       < SelectMonedas />
+
+      {/* 2 */}
+      <SelectCriptomonedas />
 
       <InputSubmit
         type="submit"
@@ -78,22 +71,9 @@ const Formulario = () => {
 export default Formulario
 
 /* 
-      Creando un array con la informacion que queremos de la API
+    Incorporando un segundo Select con el hook useSelectMonedas()
 
-  1.- Guardamos lo que hay CoinInfo, dentro del array "arrayCripto"
+  1.- Hacemos practicamente una copia de la declracion de moneda, pero en este caso para la criptomoneda
 
-  2.- Luego de verificar que lo que quiero me lo esta mostrando.
-  
-  3.- Creo un objeto literal con la info de Name y FullNme, que esta en la API, bajo el mismo esquema que se diseño monedas (monedas.js)
-
-  4.- Retornamos el objeto, ya que no estamos trabajando con return implicito
-
-  5.- Hacemos un CLG del array (arrayCripto), para ir verificando que tenemos la informacion
-      estructurada en nuestro obejto con la info de la Api
-
-  6.- Con esta informacin (el array de criptos), debemos guardarlo en un State, lo importamos
-
-  7.- Lo inicializamos.
-
-  8.- Le pasamos a la funcion de setCriptos el array que sacamos de la API
+  2.- Renderizamos el Select (Recuerda que esta definido como una funcion que retorna un html)
 */
